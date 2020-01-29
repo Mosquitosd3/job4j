d4945d732c3873700e387b9992ecb2c21406d7b6
Mosquitosd <Mosquitod3@yandex.ru>
2020-01-29 20:01:57 +0400

    Создать программу, вычисляющую факториал [#225509]

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+@Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty(){
+       int rsl = Factorial.cal(5);
+       int expected = 120;
+       assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCalculateFactorialZerohenOne(){
+    int rsl = Factorial.cal(0);
+        int expected = 1;
+        assertThat(rsl, is(expected));
+    }
+}
