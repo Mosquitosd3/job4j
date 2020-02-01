d3c0449f67f992eb50ebf9ecf5774954ab1b3421
Mosquitosd <Mosquitod3@yandex.ru>
2020-02-01 15:48:03 +0400

    JavaDoc. Code convention[#225511]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculator task.
+ *
+ * @author Nikolya Gorbunkov (Mosquitod3@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculator;
--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,6 +2,9 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int finish) {
+        if (finish == 1) {
+            return false;
+        }
         for (int index = 2; index < finish; index++) {
             if (finish % index == 0) {
                 return false;
