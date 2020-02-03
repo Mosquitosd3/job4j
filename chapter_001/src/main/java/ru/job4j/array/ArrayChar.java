2bd94c61821c58d1c4691c96b0ddfb8f97492295
Mosquitosd <Mosquitod3@yandex.ru>
2020-02-03 11:16:27 +0400

    Слово начинается с ...[#225446]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,16 +2,12 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startWith(char[] word, char[] pref){
-        int count = 0;
         for (int index = 0; index < pref.length; index++) {
-            if(pref[index] == word[index]) {
-                count++;
+            if(pref[index] != word[index]) {
+                return false;
             }
         }
-        if (count == pref.length) {
-            return true;
-        }
-        return false;
+        return true;
     }
 
 }
