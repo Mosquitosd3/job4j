11cef965fb69b4cd9ffb0ce1e22c6e3cf3dfb7fa
Mosquitosd <Mosquitod3@yandex.ru>
2020-02-16 13:58:15 +0400

    Экзамен. Базовый синтаксис.[#225498]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -0,0 +1,50 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int rslIndex = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] <= right[rightIndex]) {
+                rsl[rslIndex] = left[leftIndex];
+                leftIndex++;
+            } else {
+                rsl[rslIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            rslIndex++;
+        }
+
+        if (leftIndex < left.length) {
+            for (; leftIndex < left.length; leftIndex++) {
+                rsl[rslIndex] = left[leftIndex];
+                rslIndex++;
+            }
+        }
+
+        if (rightIndex < right.length) {
+            for (; rightIndex < right.length; rightIndex++) {
+                rsl[rslIndex] = right[rightIndex];
+                rslIndex++;
+            }
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
