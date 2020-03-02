644689153b4035e61450e3c88a95401b2cb88d4e
Mosquitosd <Mosquitod3@yandex.ru>
2020-03-02 21:25:17 +0400

    1.2. Вызов метода объекта[#225602]

---
+++ chapter_002/src/main/java/oop/Student.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class Student {
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+    }
+
+    public void music() {
+        System.out.println("I believe I can fly");
+    }
+}
