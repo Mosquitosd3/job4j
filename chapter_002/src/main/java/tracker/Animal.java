f223a9fb3c274393307557b7753320d67be81645
Mosquitosd <Mosquitod3@yandex.ru>
2020-03-07 18:10:49 +0400

    2. Перегрузка конструктора.[#225612]

--- chapter_002/src/main/java/tracker/Animal.java
+++ chapter_002/src/main/java/tracker/Animal.java
@@ -1,9 +1,14 @@
 package tracker;
 
 public class Animal {
+    private String name;
     public Animal() {
         super();
         System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
     }
 
     public static void main(String[] args) {
