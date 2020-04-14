ec1ee524e167733a6c6422e91a88e707c9e2acd1
Mosquitosd <Mosquitod3@yandex.ru>
2020-04-14 19:04:29 +0400

    0.4. Пользовательские исключения.[#225589] refactoring

---
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rls = -1;
+        int index = 0;
+        for (String str: value) {
+            if (str.equals(key)) {
+                rls = index;
+                break;
+            }
+            index++;
+        }
+        if (rls == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rls;
+    }
+
+    public static void main(String[] args) {
+        String[] str = {"kolya", "diana", "masha"};
+        try {
+            indexOf(str, "ivan");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
