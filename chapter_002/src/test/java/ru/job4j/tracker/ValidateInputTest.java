cc746ceac972fbf2d7a776420e9a131e97d07095
Mosquitosd <Mosquitod3@yandex.ru>
2020-04-16 20:13:10 +0400

    1.1. Тесты на ValidateInput.[#225592]

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -7,11 +9,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ValidateInputTest {
+    private final ByteArrayOutputStream mem = new ByteArrayOutputStream();
+    private final PrintStream out = System.out;
+
+    @Before
+    public void loadOutputStream() {
+        System.setOut(new PrintStream(this.mem));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.out);
+    }
+
     @Test
     public void whenInvalidInput() {
-        ByteArrayOutputStream mem = new ByteArrayOutputStream();
-        PrintStream out = System.out;
-        System.setOut(new PrintStream(mem));
         String[] data = {"one", "1"};
         ValidateInput input = new ValidateStubInput(data);
         input.askInt("Enter");
@@ -24,9 +36,6 @@ public class ValidateInputTest {
 
     @Test
     public void whenMoreMaxNumber() {
-        ByteArrayOutputStream mem = new ByteArrayOutputStream();
-        PrintStream out = System.out;
-        System.setOut(new PrintStream(mem));
         String[] data = {"3", "1"};
         ValidateInput input = new ValidateStubInput(data);
         input.askInt("Enter", data.length);
