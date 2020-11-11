95e46e7acc37eaf45104489d862fe4880694d009
Mosquito <Mosquitod3@yandex.ru>
2020-11-11 20:17:20 +0300

    Сортировка номера. [#225529] refactoring

--- chapter_002/src/main/java/ru/job4j/collection/LexSort.java
+++ chapter_002/src/main/java/ru/job4j/collection/LexSort.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String[] leftNumber = left.split(".");
-        String[] rightNumber = right.split(".");
+        String[] leftNumber = left.split("\\.");
+        String[] rightNumber = right.split("\\.");
         return Integer.compare(Integer.parseInt(leftNumber[0]), Integer.parseInt(rightNumber[0]));
     }
 }
